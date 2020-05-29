44db17803241dc24c469bd81620c3dec57ded0a2
shannonphu <shannonphu@gmail.com>
2020-05-28 21:37:47 -0700

    Fixed linter errors

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,16 +1,16 @@
 'use strict';
 
 function index(req, res) {
-    res.json({ data: 'Hello World!' })
+    res.json({ data: 'Hello World!' });
 }
 
 function getName(req, res) {
-    res.json({ data: 'Joe Bruin' })
+    res.json({ data: 'Joe Bruin' });
 }
 
 function editName(req, res) {
     let name = req.body.name;
-    res.json({ data: name })
+    res.json({ data: name });
 }
 
 module.exports = {
