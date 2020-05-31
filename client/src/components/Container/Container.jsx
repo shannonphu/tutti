d048518873d540e06717ba665de39e389d939eb3
shannonphu <shannonphu@gmail.com>
2020-05-30 20:33:14 -0700

    Clean up

--- client/src/components/Container/Container.jsx
+++ client/src/components/Container/Container.jsx
@@ -2,22 +2,6 @@ import React, { Component } from 'react';
 import { DummyPost, DummyChat } from '..';
 
 class Container extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.submitHandler = this.submitHandler.bind(this);
-        // This calls the API to get an initial username
-        props.getUsername();
-    }
-
-    submitHandler(event) {
-        event.preventDefault();
-        let name = this.refs.name.value;
-        // This 
-        this.props.addUsername(name);
-        this.refs.name.value = null;
-    }
-
     render() {
         return (
             <div>
