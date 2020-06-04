1510de427183bb0b447b3945b77cfa7093eb602a
shannonphu <shannonphu@gmail.com>
2020-06-03 22:50:09 -0700

    Only record when user prompts

--- client/src/components/Microphone/Microphone.jsx
+++ client/src/components/Microphone/Microphone.jsx
@@ -8,6 +8,7 @@ class Microphone extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            hasAccessToMicrophone: false,
             isRecording: false,
             blobData: null,
             blobUri: null
@@ -17,27 +18,45 @@ class Microphone extends Component {
 
         this.mic = new Tone.UserMedia();
         this.mic.connect(Tone.Master);
+        this.mediaRecorder = null;
 
         this.handleClick = this.handleClick.bind(this);
+        this.startMicrophonePermissions = this.startMicrophonePermissions.bind(this);
+        this.stopMicrophoneAccess = this.stopMicrophoneAccess.bind(this);
         this.startRecording = this.startRecording.bind(this);
         this.stopRecording = this.stopRecording.bind(this);
         this.saveAudio = this.saveAudio.bind(this);
     }
 
-    async componentDidMount() {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        this.mediaRecorder = new MediaRecorder(stream);
-        this.mediaRecorder.ondataavailable = e => {
-            if (e.data && e.data.size > 0) {
-                this.chunks.push(e.data);
-            }
-        };
+    startMicrophonePermissions() {
+        navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+            window.streamReference = stream;
+
+            this.setState({ hasAccessToMicrophone: true });
+
+            this.mediaRecorder = new MediaRecorder(stream);
+            this.mediaRecorder.ondataavailable = e => {
+                if (e.data && e.data.size > 0) {
+                    this.chunks.push(e.data);
+                }
+            };
+        })
+    }
+
+    stopMicrophoneAccess() {
+        window.streamReference.getAudioTracks().forEach((track) => {
+            track.stop();
+        });
     }
 
     startRecording() {
+        // Ask the user to activate their mic
+        if (!this.state.hasAccessToMicrophone) {
+            this.startMicrophonePermissions();
+        }
+        
         this.chunks = [];
 
-        // Ask the user to activate their mic
         this.mic.open().then(() => {
             Tone.Transport.start();
             this.mediaRecorder.start(5);
@@ -46,6 +65,8 @@ class Microphone extends Component {
     }
 
     stopRecording() {
+        this.setState({ hasAccessToMicrophone: false });
+        this.stopMicrophoneAccess();
         this.mediaRecorder.stop();
         this.mic.close();
         Tone.Transport.stop();
