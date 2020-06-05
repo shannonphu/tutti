397da7c7edd0cde53e5de1e3a0abaaf3e66b475d
shannonphu <shannonphu@gmail.com>
2020-06-05 15:13:24 -0700

    integrate peakjs with tonejs

--- client/src/components/AudioWaveform/AudioWaveform.jsx
+++ client/src/components/AudioWaveform/AudioWaveform.jsx
@@ -4,7 +4,6 @@ import Peaks from 'peaks.js';
 import IconButton from '@material-ui/core/IconButton';
 import MicIcon from '@material-ui/icons/Mic';
 import MicOffIcon from '@material-ui/icons/MicOff';
-import { white } from 'color-name';
 import SAMPLE_MP3 from '../../assets/kevin_bossa.mp3';
 
 class AudioWaveform extends Component {
@@ -13,56 +12,146 @@ class AudioWaveform extends Component {
         this.state = {};
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         const AudioContext = window.AudioContext || window.webkitAudioContext;
         const audioContext = new AudioContext();
 
-        var options = {
-            containers: {
-                // 'zoomview': document.getElementById('zoomview'),
-                'overview': document.getElementById('overview'),
-            },
-            mediaElement: document.getElementById('audio'),
-            webAudio: {
-                audioContext: audioContext
-            },
-            overviewWaveformColor: 'green',
-            height: 120,
-            showPlayheadTime: true,
-            // zoomLevels: [512, 1024, 2048, 4096],
-            keyboard: true,
-            segments: [
-                {
-                    startTime: 1,
-                    endTime: 3,
-                    editable: true,
-                    color: 'blue',
-                    labelText: 'part1'
-                },
-                {
-                    startTime: 3,
-                    endTime: 5,
-                    editable: true,
-                    color: 'purple',
-                    labelText: 'part2'
-                }
-            ]
-        };
+        await fetch(SAMPLE_MP3)
+            .then(function (response) {
+                return response.arrayBuffer();
+            })
+            .then(function (buffer) {
+                return audioContext.decodeAudioData(buffer);
+            })
+            .then(function (audioBuffer) {
+                const player = {
+                    externalPlayer: new Tone.Player(audioBuffer),
+                    eventEmitter: null,
 
-        Peaks.init(options, (err, peaks) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(peaks)
-                document.getElementById('zoomIn').addEventListener('click', function () {
-                    peaks.zoom.zoomIn();
-                });
+                    init: function (eventEmitter) {
+                        this.eventEmitter = eventEmitter;
 
-                document.getElementById('zoomOut').addEventListener('click', function () {
-                    peaks.zoom.zoomOut();
+                        this.externalPlayer.sync();
+                        this.externalPlayer.start();
+
+                        Tone.connectSeries(this.externalPlayer, Tone.Master);
+
+                        eventEmitter.emit('player.canplay');
+
+                        Tone.Transport.scheduleRepeat(() => {
+                            var time = this.getCurrentTime();
+                            eventEmitter.emit('player.timeupdate', time);
+
+                            if (time >= this.getDuration()) {
+                                Tone.Transport.stop();
+                            }
+                        }, 0.25);
+                    },
+
+                    destroy: function () {
+                        Tone.context.dispose();
+                        this.externalPlayer = null;
+                        this.eventEmitter = null;
+                    },
+
+                    play: function () {
+                        Tone.Transport.start(
+                            Tone.now(),
+                            this.getCurrentTime()
+                        );
+
+                        this.eventEmitter.emit('player.play', this.getCurrentTime());
+                    },
+
+                    pause: function () {
+                        Tone.Transport.pause();
+                        this.eventEmitter.emit('player.pause', this.getCurrentTime());
+                    },
+
+                    isPlaying: function () {
+                        return Tone.Transport.state === "started";
+                    },
+
+                    seek: function (time) {
+                        Tone.Transport.seconds = time;
+
+                        this.eventEmitter.emit('player.seeked', this.getCurrentTime());
+                        this.eventEmitter.emit('player.timeupdate', this.getCurrentTime());
+                    },
+
+                    isSeeking: function () {
+                        return false;
+                    },
+
+                    getCurrentTime: function () {
+                        return this.externalPlayer.buffer.toSeconds(Tone.Transport.position);
+                    },
+
+                    getDuration: function () {
+                        return this.externalPlayer.buffer.duration;
+                    }
+                };
+                var options = {
+                    containers: {
+                        zoomview: document.getElementById('zoomview'),
+                        overview: document.getElementById('overview')
+                    },
+                    height: 150,
+                    player: player,
+                    webAudio: {
+                        audioBuffer: audioBuffer,
+                        scale: 128,
+                        multiChannel: false
+                    },
+                    keyboard: true,
+                    showPlayheadTime: true,
+                    zoomLevels: [128, 256, 512, 1024, 2048, 4096]
+                };
+
+                Peaks.init(options, function (err, peaksInstance) {
+                    if (err) {
+                        console.error(err.message);
+                        return;
+                    }
+
+                    console.log('Peaks instance ready');
+
+                    document.querySelector('[data-action="play"]').addEventListener('click', function () {
+                        peaksInstance.player.play();
+                    });
+
+                    document.querySelector('[data-action="pause"]').addEventListener('click', function () {
+                        peaksInstance.player.pause();
+                    });
+
+                    document.querySelector('button[data-action="seek"]').addEventListener('click', function (event) {
+                        var time = document.getElementById('seek-time').value;
+                        var seconds = parseFloat(time);
+
+                        if (!Number.isNaN(seconds)) {
+                            peaksInstance.player.seek(seconds);
+                        }
+                    });
+
+                    document.querySelector('[data-action="zoom-in"]').addEventListener('click', function () {
+                        peaksInstance.zoom.zoomIn();
+                    });
+
+                    document.querySelector('[data-action="zoom-out"]').addEventListener('click', function () {
+                        peaksInstance.zoom.zoomOut();
+                    });
+
+                    document.querySelector('[data-action="play-segment"]').addEventListener('click', function () {
+                        var start = document.getElementById('segment-start').value;
+                        var startInSeconds = parseFloat(start);
+
+                        var end = document.getElementById('segment-end').value;
+                        var endInSeconds = parseFloat(end);
+
+                        peaksInstance.player.playSegment({ startTime: startInSeconds, endTime: endInSeconds, editable: true });
+                    });
                 });
-            }
-        });
+            });
     }
 
     render() {
@@ -70,15 +159,18 @@ class AudioWaveform extends Component {
             <div>
                 <div id='zoomview' style={{ backgroundColor: 'white', borderColor: 'grey' }}></div>
                 <div id='overview' style={{ backgroundColor: 'white', borderColor: 'grey' }}></div>
-                <audio id='audio' controls='controls'>
-                    <source src={SAMPLE_MP3} type='audio/mp3' />
-                    Your browser does not support the audio element.
-                </audio>
-                <div id='controls'>
-                    <button id='zoomIn'>Zoom In</button>
-                    <button id='zoomOut'>Zoom Out</button>
-                    <button id='segment'>Add Segment</button>
-                    <button id='point'>Add Point</button>
+                <div id="demo-controls">
+                    <div id="controls">
+                        <button data-action="play">Play</button>
+                        <button data-action="pause">Pause</button>
+                        <input type="text" id="seek-time" value="0.0" />
+                        <button data-action="seek">Seek</button>
+                        <button data-action="zoom-in">Zoom in</button>
+                        <button data-action="zoom-out">Zoom out</button>
+                        <input type="text" id="segment-start" value="2.0" />
+                        <input type="text" id="segment-end" value="5.0" />
+                        <button data-action="play-segment">Play segment</button>
+                    </div>
                 </div>
             </div>
         )
