073273fdcec9e48a5d9acba3f80874ad51591b94
shannonphu <shannonphu@gmail.com>
2020-06-06 13:01:23 -0700

    load recordings into table

--- client/src/components/AudioWaveform/AudioWaveform.jsx
+++ client/src/components/AudioWaveform/AudioWaveform.jsx
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import Peaks from 'peaks.js';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import StopIcon from '@material-ui/icons/Stop';
-import ZoomInIcon from '@material-ui/icons/ZoomIn';
-import ZoomOutIcon from '@material-ui/icons/ZoomOut';
 import { Player } from './Player';
 import SAMPLE_MP3 from '../../assets/kevin_bossa.mp3';
 
@@ -17,22 +14,25 @@ class AudioWaveform extends Component {
             isPlaying: false 
         };
 
-        this.togglePlay = this.togglePlay.bind(this);
-        this.zoomIn = this.zoomIn.bind(this);
-        this.zoomOut = this.zoomOut.bind(this);
-        this.seek = this.seek.bind(this);
-        this.playSegment = this.playSegment.bind(this);
+        this.play = this.play.bind(this);
+        this.pause = this.pause.bind(this);
 
         const AudioContext = window.AudioContext || window.webkitAudioContext;
         this.audioContext = new AudioContext();
     }
 
     async componentDidMount() {
+        let audioName = this.props.audioName;
+        let audioUrl = this.props.audioUrl;
+        let height = this.props.height;
         let _component = this;
 
-        await fetch(SAMPLE_MP3)
+        await fetch(audioUrl)
             .then((response) => {
-                return response.arrayBuffer();
+                return response.blob();
+            })
+            .then((blob) => {
+                return new Response(blob).arrayBuffer()
             })
             .then((buffer) => {
                 return this.audioContext.decodeAudioData(buffer);
@@ -40,10 +40,11 @@ class AudioWaveform extends Component {
             .then((audioBuffer) => {
                 var options = {
                     containers: {
-                        zoomview: this.refs.zoomView,
-                        overview: this.refs.overView
+                        // zoomview: this.refs.zoomView,
+                        overview: this.refs[`overView-${audioName}`]
                     },
-                    height: 150,
+                    height: height,
+                    overviewWaveformColor: 'navy',
                     player: new Player(audioBuffer),
                     webAudio: {
                         audioBuffer: audioBuffer,
@@ -55,7 +56,7 @@ class AudioWaveform extends Component {
                     zoomLevels: [128, 256, 512, 1024, 2048, 4096]
                 };
 
-                Peaks.init(options, function (err, peaksInstance) {
+                Peaks.init(options, (err, peaksInstance) => {
                     if (err) {
                         console.error(err.message);
                         return;
@@ -66,64 +67,31 @@ class AudioWaveform extends Component {
             });
     }
 
-    togglePlay() {
-        if (this.state.isPlaying) {
-            this.state.peaks.player.pause();
-        } else {
-            this.state.peaks.player.play();
-        }
-
-        this.setState({ isPlaying: !this.state.isPlaying });
+    play() {
+        this.state.peaks.player.play();
     }
 
-    zoomIn() {
-        this.state.peaks.zoom.zoomIn();
-    }
-
-    zoomOut() {
-        this.state.peaks.zoom.zoomOut();
-    }
-
-    // Demo implementation. Might remove.
-    seek() {
-        var time = this.refs.seekTime.value;
-        var seconds = parseFloat(time);
-        if (!Number.isNaN(seconds)) {
-            this.state.peaks.player.seek(seconds);
-        }
-    }
-
-    // Demo implementation. Might remove.
-    playSegment() {
-        var start = this.refs.segmentStartTime.value;
-        var startInSeconds = parseFloat(start);
-        var end = this.refs.segmentEndTime.value;
-        var endInSeconds = parseFloat(end);
-        this.state.peaks.player.playSegment({ startTime: startInSeconds, endTime: endInSeconds, editable: true });
+    pause() {
+        this.state.peaks.player.pause();
     }
 
     render() {
         return (
             <div>
-                <div id='zoomview' ref='zoomView' style={{ backgroundColor: 'white', borderColor: 'grey' }}></div>
-                <div id='overview' ref='overView' style={{ backgroundColor: 'white', borderColor: 'grey' }}></div>
-                {this.state.peaks ? 
+                <div ref={`zoomView-${this.props.audioName}`} style={{ backgroundColor: 'white', borderColor: 'grey' }}></div>
+                <div ref={`overView-${this.props.audioName}`} style={{ backgroundColor: 'white', borderColor: 'grey' }}></div>
+
+                {this.state.peaks && this.props.shouldShowControls ? 
                     <div>
-                        <IconButton onClick={this.togglePlay}>
-                            {this.state.isPlaying ? <StopIcon /> : <PlayCircleFilledIcon />}
-                        </IconButton>
-                        <IconButton onClick={this.zoomIn}>
-                            <ZoomInIcon />
-                        </IconButton>
-                        <IconButton onClick={this.zoomOut}>
-                            <ZoomOutIcon />
-                        </IconButton>
-                        <input type='text' defaultValue='0.0' ref='seekTime' />
-                        <Button onClick={this.seek}>Seek</Button>
-                        <input type='text' defaultValue='2.0' ref='segmentStartTime' />
-                        <input type='text' defaultValue='5.0' ref='segmentEndTime' />
-                        <Button onClick={this.playSegment}>Play segment</Button>
-                    </div> 
+                        <div>
+                            <IconButton onClick={this.play}>
+                                <PlayCircleFilledIcon />
+                            </IconButton>
+                            <IconButton onClick={this.pause}>
+                                <StopIcon />
+                            </IconButton>
+                        </div> 
+                    </div>
                 : null}
             </div>
         )
