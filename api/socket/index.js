9d3753b6068e40335064d751bbe2fd94162fa09d
Allen Qin <me2myself3andi@gmail.com>
2020-06-07 15:41:01 -0700

    added backend to send special loope daudio to server

--- api/socket/index.js
+++ api/socket/index.js
@@ -73,12 +73,6 @@ const SocketRouter = function (server, cache) {
             case 'socket/UPLOAD_LOOPED_AUDIO':
                 onUploadLoopedAudio(action);
                 break;
-            case 'socket/SET_BASELINE_PLAYER':
-                onSetBaselinePlayer(action);
-                break;
-            case 'socket/PLAYER_UPDATED_RECORDING_STATE':
-                onPlayerUpdatedRecordingState(action);
-                break;
             default:
                 break;
         }
