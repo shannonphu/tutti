119c4084ae1f4cd0a5c7fe6fe3a6970b23fd82f4
Allen Qin <me2myself3andi@gmail.com>
2020-06-06 20:25:16 -0700

    add a loop event to the looper

--- client/src/components/Looper/Looper.jsx
+++ client/src/components/Looper/Looper.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import { withStyles } from '@material-ui/core/styles';
 import Tone from 'tone';
 import LoopIcon from '@material-ui/icons/Loop';
 import Button from '@material-ui/core/Button';
@@ -9,30 +10,48 @@ class Looper extends Component {
         super(props);
         this.state = {
             isLoaded: false,
-            isPlaying: false
         };
+        Tone.Transport.bpm.value = this.props.room.bpm;
         this.player = new Tone.Player(SAMPLE_MP3).toMaster();
-        this.player.loop = true;
+        this.player.retrigger = true;
+
         Tone.Buffer.on('load', 
-            () => {this.setState({...this.state, isLoaded: true})
-        })
+            () => {this.setState({...this.state, isLoaded: true});}
+        );
         
         this.handleOnClick = this.handleOnClick.bind(this);
+        this.playAudioCallback = this.playAudioCallback.bind(this);
+
+        this.toneNumBars = Tone.Time(this.props.room.numBars, 'm');
+        this.toneTotalBars = Tone.Time(this.props.room.numBars * this.props.room.numLoops, 'm')
+        this.looper = new Tone.Event((this.playAudioCallback), this.toneNumBars);
+        this.looper.loop = this.props.room.numLoops;
+        this.looper.loopEnd = this.toneNumBars;
+
     }
+    
+    playAudioCallback(time, duration) {
+        this.player.start(time).stop(time+duration);
+    }
+
 
     handleOnClick(event) {
         event.preventDefault();
-        console.log(this.state.isLoaded)
-        console.log(this.player.buffer)
-        if (!(this.state.isPlaying)) {
-            this.player.start()
-            console.log("start")
-            this.setState({...this.state, isPlaying: true})
+        console.log(this.looper.state)
+        if (this.looper.state == 'stopped') {
+            this.looper.start(0); 
+            this.looper.stop(this.toneTotalBars);
+            Tone.Transport.seconds = 0; // restart
+        }
+        else if (this.looper.state == 'started') {
+            this.looper.cancel();
+            this.player.stop();
         }
         else {
-            this.player.stop()
-            console.log("stop")
-            this.setState({...this.state, isPlaying: false})
+            console.log('state is busted');
+        }
+        if (Tone.Transport.state == 'stopped') {
+            Tone.Transport.start();
         }
     }
 
