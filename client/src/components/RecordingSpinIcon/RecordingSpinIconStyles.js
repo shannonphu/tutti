de5a15b3c4e49e91697296ce2293b0f60ea5a8d3
shannonphu <shannonphu@gmail.com>
2020-06-13 22:39:57 -0700

    Fix recording button style bug

--- client/src/components/RecordingSpinIcon/RecordingSpinIconStyles.js
+++ client/src/components/RecordingSpinIcon/RecordingSpinIconStyles.js
@@ -1,12 +1,13 @@
 export default (theme) => ({
     root: {
         paddingTop: 5.5,
-        paddingLeft: 0
+        paddingLeft: 0,
+        position: 'relative'
     },
     progress: {
         position: 'absolute',
-        top: 17.5,
-        left: 12,
+        top: 6,
+        left: 0,
         zIndex: 1,
         color: theme.palette.error.main
     }
